cebd62187d0b029e711f733e333803b631950783
AlejoFiesco <alfiesparra@hotmail.com>
2022-07-03 20:01:33 -0500

    Boceto 0.1.1
    Mejorado el scroll reveal

--- servicios.php
+++ servicios.php
@@ -27,6 +27,14 @@
             </div>
         </div>
     </div>
+    <div class="fondo">
+        <div class="servicio">
+            <div class="izquierda">
+                <h1 class="expandir">Servicio 4</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto, magni omnis amet soluta quaerat laudantium delectus quidem voluptatum qui! Dolorum assumenda eius illo, excepturi distinctio unde nihil fugit numquam quam error cum placeat magnam officiis quod velit! Inventore nostrum vero exercitationem fugit beatae magni praesentium vel voluptas, illo earum odit cumque hic quas deleniti? Accusantium inventore non ex impedit placeat.</p>
+            </div>
+        </div>
+    </div>
 </div>
 <script src="https://unpkg.com/scrollreveal"></script>
 <script src="./src/js/servicios.js"></script>
--- src/js/servicios.js
+++ src/js/servicios.js
@@ -1,3 +1,13 @@
 ScrollReveal().reveal('.fondo');
-ScrollReveal().reveal('.servicio h1');
-ScrollReveal().reveal('.servicio p');
+ScrollReveal().reveal('.derecha h1, .derecha p', ({
+    delay: 50,
+    reset: true,
+    distance: '30px',
+    origin: 'right'
+}));
+ScrollReveal().reveal('.izquierda h1, .izquierda p', ({
+    delay: 50,
+    reset: true,
+    distance: '30px',
+    origin: 'left'
+}));
